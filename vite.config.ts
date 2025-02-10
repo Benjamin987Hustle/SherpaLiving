dcf3b6372ce96e4c6790d784a753157e8796208d
BenHustle987 <benjamin.egretaud@hec.ca>
2025-02-10 10:57:43 -0500

    On a mis une virgule stp marche

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Remplace 'SherpaLiving' par le nom exact de ton repo GitHub
 export default defineConfig({
-  base: '/SherpaLiving/', // IMPORTANT : Ajoute le chemin correct pour GitHub Pages
+  base: '/SherpaLiving/', // Assure-toi que le nom correspond à ton repo GitHub
   plugins: [react()],
-  base: '/'
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
